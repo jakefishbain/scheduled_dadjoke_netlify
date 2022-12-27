3c1b4650a050830f940e57846df776e7f3a60292
jakefishbain <jakefishbain@gmail.com>
2022-12-27 14:05:43 -0600

    add joke to hello world

--- netlify/functions/hello-world.ts
+++ netlify/functions/hello-world.ts
@@ -1,6 +1,17 @@
+import fetch from 'node-fetch';
+
 export const handler = async () => {
+  const joke = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+        Accept: 'text/plain',
+    },
+  }).then((res) => res.text());
+
+  console.log('JOKE: ', joke)
+
   return {
     statusCode: 200,
-    body: 'waddup chat?',
+    body: JSON.stringify(joke, null, 2)
+    // body: 'waddup chat?',
   };
 };
